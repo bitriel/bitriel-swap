2ae9eee6a5dbae1ef66e420a85a2dd53a8f7ffa1
veasnama <maveasnatech@gmail.com>
2021-06-12 16:05:26 +0700

    Deployed and Verify Token

--- contracts/SELToken.sol
+++ contracts/SELToken.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.3;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
 contract SELToken is ERC20 {
   constructor() ERC20('SELToken', 'SEL') {
     _mint(msg.sender, 1000000 * 10 ** 18);
   }
 }
+
--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
+// const LedgerWalletProvider = require('truffle-ledger-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const secrets = JSON.parse(
+  fs.readFileSync('.secret.json').toString().trim()
+);
 module.exports = {
 	/**
 	 * Networks define how you connect to your ethereum client and let you set the
@@ -34,19 +14,31 @@ module.exports = {
 	 *
 	 * $ truffle test --network <network-name>
 	 */
-
+	plugins: [
+		'truffle-plugin-verify'	
+	],
+	api_keys: {
+		bscscan: secrets.api_key
+	},
 	networks: {
-		// Useful for testing. The `development` name is special - truffle uses it by default
-		// if it's defined here and no other network is specified at the command line.
-		// You should run a client (like ganache-cli, geth or parity) in a separate terminal
-		// tab if you use this network and you must also set the `host`, `port` and `network_id`
-		// options below to some value.
-		//
-		development: {
-			host: "127.0.0.1",     // Localhost (default: none)
-			port: 7545,            // Standard Ethereum port (default: none)
-			network_id: "*",       // Any network (default: none)
-		}
+		// development: {
+		// 	host: "127.0.0.1",     // Localhost (default: none)
+		// 	port: 7545,            // Standard Ethereum port (default: none)
+		// 	network_id: "*",       // Any network (default: none)
+		// },
+		bscTestnet: {
+
+			provider: () => new HDWalletProvider(
+   		    secrets.seed,
+        	'https://data-seed-prebsc-1-s1.binance.org:8545/'
+      		),
+      		network_id: 97,
+      		skipDryRun: true,
+			timeoutBlocks: 200,
+			confirmations: 5,
+			production: true
+   		}
+		
 		// Another network with more advanced options...
 		// advanced: {
 		// port: 8777,             // Custom port
@@ -72,11 +64,12 @@ module.exports = {
 		// network_id: 2111,   // This network is yours, in the cloud.
 		// production: true    // Treats this network as if it was a public net. (default: false)
 		// }
+
 	},
 
 	// Set default mocha options here, use special reporters etc.
 	mocha: {
-		// timeout: 100000
+		timeout: 100000
 	},
 
 	// Configure your compilers
@@ -84,16 +77,15 @@ module.exports = {
 		solc: {
 			version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
 			// docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-			// settings: {          // See the solidity docs for advice about optimization and evmVersion
-			//  optimizer: {
-			//    enabled: false,
-			//    runs: 200
-			//  },
-			//  evmVersion: "byzantium"
-			// }
+			settings: {          // See the solidity docs for advice about optimization and evmVersion
+			 optimizer: {
+			   enabled: false,
+			   runs: 200
+			 },
+			 evmVersion: "byzantium"
+			}
 		}
 	},
-
 	// Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
 	//
 	// Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
